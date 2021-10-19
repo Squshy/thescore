d79f4072c793f4450638cac3ca664a489a5a2b90
Unknown <Calvin.lapp@dcmail.ca>
2021-10-19 16:53:23 -0400

    responsive stuff

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -17,8 +17,8 @@ export const Header: React.FC<HeaderProps> = ({}) => {
       </Head>
       <div className="flex items-center py-4 px-4 border-b border-gray-700 justify-between h-16">
         <div className="flex items-center">
-          <p className="text-3xl mr-2 font-light">NFL Rushing</p>
-          <FlagIcon className="w-6 h-6 text-blue-400" />
+          <p className="text-2xl md:text-3xl mr-2 font-light">NFL Rushing</p>
+          <FlagIcon className="w-4 h-4 md:w-6 md:h-6 text-blue-400" />
         </div>
         <div className="relative text-gray-600 focus-within:text-gray-400">
           <span className="absolute inset-y-0 left-0 flex items-center pl-2">
@@ -26,13 +26,13 @@ export const Header: React.FC<HeaderProps> = ({}) => {
               type="submit"
               className="p-1 focus:outline-none focus:shadow-outline"
             >
-              <SearchIcon className="w64 h-6" />
+              <SearchIcon className="w-6 h-6" />
             </button>
           </span>
           <input
             type="search"
             name="search"
-            className="transition duration-150 ease-out py-2 text-sm text-white bg-gray-800 rounded-md pl-10 focus:outline-none focus:bg-white focus:text-gray-900"
+            className="transition duration-150 ease-out py-2 text-sm text-white bg-gray-800 rounded-md pl-10 focus:outline-none focus:bg-white focus:text-gray-900 md:w-64"
             placeholder="Search players..."
             autoComplete="off"
           />
