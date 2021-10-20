12f6dbc1fd7f33c9b0f1ef6c7790de41db592532
Unknown <Calvin.lapp@dcmail.ca>
2021-10-20 16:36:24 -0400

    no data display screen

--- client/components/table/RushesDisplay.tsx
+++ client/components/table/RushesDisplay.tsx
@@ -12,7 +12,7 @@ export const RushesDisplay: React.FC<RushesDisplayProps> = ({
   rushes,
   sortData,
 }) => {
-  if (rushes.length <= 0) return <div>No data to display</div>;
+  if (rushes.length <= 0) return <div className="w-full bg-white bg-opacity-5 p-4 rounded-sm text-gray-500 text-center text-lg font-thin shadow-md">No data to display.</div>;
   return (
     <div className="overflow-x-auto overflow-y-auto">
       <table className="min-w-full divide-y divide-gray-600 overflow-x-auto">
